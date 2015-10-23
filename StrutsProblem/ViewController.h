2936e1e0a69615e5509b6e187e6ea8132090a60c
kwana008 <Andres.X.Kwan.-ND@Disney.com>
2015-10-23 17:14:23 -0500

    Initial Commit

---
+++ StrutsProblem/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StrutsProblem
+//
+//  Created by Andres Kwan on 10/23/15.
+//  Copyright © 2015 Kwan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ StrutsProblem/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  StrutsProblem
+//
+//  Created by Andres Kwan on 10/23/15.
+//  Copyright © 2015 Kwan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
