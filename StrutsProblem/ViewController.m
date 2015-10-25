725fa4e9683edab97491aea57cbddd9d04c90531
ANDRES KWAN <andreskwan@gmail.com>
2015-10-25 09:56:23 -0500

    Added - understand that Structs are pass by value - UIView "frame" to position a view in superview coordinated system! - RM, BM - autoresize work - CoreAnimation UIView "animation" dictionary and CABasicAnimation

--- StrutsProblem/ViewController.m
+++ StrutsProblem/ViewController.m
@@ -22,6 +22,7 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
+//this callback is called when the view controller is rotating to a new orientation
 - (void)viewWillLayoutSubviews
 {
     if (UIInterfaceOrientationIsLandscape([[UIApplication sharedApplication] statusBarOrientation]))
@@ -29,8 +30,11 @@
         CGRect rect = self.topLeftView.frame;
         rect.size.width = 254;
         rect.size.height = 130;
+        //Structs also passed by value in Obj-C
         self.topLeftView.frame = rect;
         
+        //Why? because here their values are changed
+        //but self.topLeftView.frame value is the assigened (copied) previously
         rect = self.topRightView.frame;
         rect.origin.x = 294;
         rect.size.width = 254;
@@ -43,7 +47,7 @@
         rect.size.height = 130;
         self.bottomView.frame = rect;
     }
-    else
+    else //Portrait also needed when comming back from landsacpe
     {
         CGRect rect = self.topLeftView.frame;
         rect.size.width = 130;
