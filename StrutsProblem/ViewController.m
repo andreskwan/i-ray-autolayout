9ee5fac3a27f4633fb19a39a537384275b880fa0
ANDRES KWAN <andreskwan@gmail.com>
2015-10-25 11:16:24 -0500

    Added - auto layout - much clear now

--- StrutsProblem/ViewController.m
+++ StrutsProblem/ViewController.m
@@ -23,48 +23,48 @@
 }
 
 //this callback is called when the view controller is rotating to a new orientation
-- (void)viewWillLayoutSubviews
-{
-    if (UIInterfaceOrientationIsLandscape([[UIApplication sharedApplication] statusBarOrientation]))
-    {
-        CGRect rect = self.topLeftView.frame;
-        rect.size.width = 254;
-        rect.size.height = 130;
-        //Structs also passed by value in Obj-C
-        self.topLeftView.frame = rect;
-        
-        //Why? because here their values are changed
-        //but self.topLeftView.frame value is the assigened (copied) previously
-        rect = self.topRightView.frame;
-        rect.origin.x = 294;
-        rect.size.width = 254;
-        rect.size.height = 130;
-        self.topRightView.frame = rect;
-        
-        rect = self.bottomView.frame;
-        rect.origin.y = 170;
-        rect.size.width = 528;
-        rect.size.height = 130;
-        self.bottomView.frame = rect;
-    }
-    else //Portrait also needed when comming back from landsacpe
-    {
-        CGRect rect = self.topLeftView.frame;
-        rect.size.width = 130;
-        rect.size.height = 254;
-        self.topLeftView.frame = rect;
-        
-        rect = self.topRightView.frame;
-        rect.origin.x = 170;
-        rect.size.width = 130;
-        rect.size.height = 254;
-        self.topRightView.frame = rect;
-        
-        rect = self.bottomView.frame;
-        rect.origin.y = 295;
-        rect.size.width = 280;
-        rect.size.height = 254;
-        self.bottomView.frame = rect;
-    }
-}
+//- (void)viewWillLayoutSubviews
+//{
+//    if (UIInterfaceOrientationIsLandscape([[UIApplication sharedApplication] statusBarOrientation]))
+//    {
+//        CGRect rect = self.topLeftView.frame;
+//        rect.size.width = 254;
+//        rect.size.height = 130;
+//        //Structs also passed by value in Obj-C
+//        self.topLeftView.frame = rect;
+//        
+//        //Why? because here their values are changed
+//        //but self.topLeftView.frame value is the assigened (copied) previously
+//        rect = self.topRightView.frame;
+//        rect.origin.x = 294;
+//        rect.size.width = 254;
+//        rect.size.height = 130;
+//        self.topRightView.frame = rect;
+//        
+//        rect = self.bottomView.frame;
+//        rect.origin.y = 170;
+//        rect.size.width = 528;
+//        rect.size.height = 130;
+//        self.bottomView.frame = rect;
+//    }
+//    else //Portrait also needed when comming back from landsacpe
+//    {
+//        CGRect rect = self.topLeftView.frame;
+//        rect.size.width = 130;
+//        rect.size.height = 254;
+//        self.topLeftView.frame = rect;
+//        
+//        rect = self.topRightView.frame;
+//        rect.origin.x = 170;
+//        rect.size.width = 130;
+//        rect.size.height = 254;
+//        self.topRightView.frame = rect;
+//        
+//        rect = self.bottomView.frame;
+//        rect.origin.y = 295;
+//        rect.size.width = 280;
+//        rect.size.height = 254;
+//        self.bottomView.frame = rect;
+//    }
+//}
 @end
