b18857315c85207d9c4302944cedd2006cdb190b
ANDRES KWAN <andreskwan@gmail.com>
2015-10-26 07:17:52 -0500

    Added project to lear more about constrains

---
+++ Constrains/Constrains/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Constrains
+//
+//  Created by Andres Kwan on 10/26/15.
+//  Copyright © 2015 Kwan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Constrains/Constrains/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Constrains
+//
+//  Created by Andres Kwan on 10/26/15.
+//  Copyright © 2015 Kwan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
